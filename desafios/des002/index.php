d46505cac28b211f5214baf4576f4462aa8c86d1
nubextremis <101215523+nubextremis@users.noreply.github.com>
2023-05-11 18:20:53 -0300

    post-get

--- desafios/des002/index.php
+++ desafios/des002/index.php
@@ -11,7 +11,7 @@
     <main>
         <h1>Trabalhando com números aleatórios</h1>
         <?php
-        $numero = rand(0, 100);
+        $numero = mt_rand(0, 100);
         echo '<p>Gerando um número aleatório entre 0 e 100...</p>';
         echo "<p>O valor gerado foi <strong>$numero</strong></p>";
         ?>
