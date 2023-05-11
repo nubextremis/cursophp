61270d9da43395395cda7a4215dbd79da6312928
nubextremis <101215523+nubextremis@users.noreply.github.com>
2023-05-11 18:28:34 -0300

    Update index.php

--- desafios/des002/index.php
+++ desafios/des002/index.php
@@ -12,10 +12,13 @@
         <h1>Trabalhando com números aleatórios</h1>
         <?php
         $numero = mt_rand(0, 100);
+        // rand()
+        // mt_rand()
+        // random_int()
         echo '<p>Gerando um número aleatório entre 0 e 100...</p>';
         echo "<p>O valor gerado foi <strong>$numero</strong></p>";
         ?>
-        <input type="button" value="↺ Gerar outro" onclick="window.location.reload()">
+        <input type="button" value="↺ Gerar outro" onclick="javascript:document.location.reload()">
     </main>
 </body>
 </html>
